91156576e96e1a94ea0ea7e878a2135eab80d146
unnati <unnati@tessact.com>
2024-08-18 22:26:01 +0530

    fix home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,34 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+import Kbd from "@/components/cmdk/Kbd";
+
+import { motion } from "framer-motion";
+
 export default function Home() {
+  const router = useRouter();
+
   return (
-    <main className="bg-stone-900 h-screen w-full p-8">
+    <main className="bg-stone-950 h-screen w-full p-8">
       {/* layout div */}
 
       <div className="flex">
-        <div className="border-2 flex items-center justify-center flex-col text-zinc-600 border-zinc-500/35 rounded-xl h-32 w-32">
-          cmd + K
-        </div>
+        <motion.div
+          whileHover={{
+            boxShadow: "inset #8c8c8c 0px 0px 60px -40px",
+          }}
+          onClick={() => router.push("/cmdk")}
+          style={{
+            boxShadow: "inset #7b7b7b 0px 0px 60px -44px",
+          }}
+          className="border-2 gap-2 flex-col cursor-pointer inline-flex shadow-lg text-md  items-center justify-center bg-zinc-900 text-zinc-500 border-zinc-800 rounded-xl h-44 w-44"
+        >
+          <p className="text-sm">Stacking modals</p>
+          <p className="text-sm">
+            <Kbd>cmd</Kbd> <Kbd>k</Kbd>
+          </p>
+        </motion.div>
       </div>
     </main>
   );
