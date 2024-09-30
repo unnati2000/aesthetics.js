d854e2fa5f51522694626580bc66099e2ce77382
Unnati Bamania <unnatibamania8@gmail.com>
2024-09-30 10:56:01 +0530

    change pnpm lock

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@
 import { useRouter } from "next/navigation";
 
 import Kbd from "@/components/cmdk/Kbd";
+import { BsTrashFill } from "react-icons/bs";
+import { CiImageOn } from "react-icons/ci";
 
 import { motion } from "framer-motion";
 
@@ -13,7 +15,7 @@ export default function Home() {
     <main className="bg-stone-950 h-screen w-full p-8">
       {/* layout div */}
 
-      <div className="flex">
+      <div className="flex gap-4">
         <motion.div
           whileHover={{
             boxShadow: "inset #8c8c8c 0px 0px 60px -40px",
@@ -29,7 +31,28 @@ export default function Home() {
             <Kbd>cmd</Kbd> <Kbd>k</Kbd>
           </p>
         </motion.div>
+
+        <motion.div
+          whileHover={{
+            boxShadow: "inset #8c8c8c 0px 0px 60px -40px",
+          }}
+          onClick={() => router.push("/delete-photos")}
+          style={{
+            boxShadow: "inset #7b7b7b 0px 0px 60px -44px",
+          }}
+          className="border-2 gap-4 flex-col cursor-pointer inline-flex shadow-lg text-md  items-center justify-center bg-zinc-900 text-zinc-500 border-zinc-800 rounded-xl h-44 w-44"
+        >
+          <p className="text-sm">Delete photos</p>
+          <div className="flex items-center relative justify-center">
+            <BsTrashFill size={32} />
+            <CiImageOn
+              className="absolute -rotate-[30deg] -left-3 -top-3"
+              size={32}
+            />
+          </div>
+        </motion.div>
       </div>
     </main>
   );
 }
+
